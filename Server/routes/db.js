8856deca275cf870e7be1fa6adcda84b31f88ede
Mohanad Ilewi <mohanad.ilewi.83@gmail.com>
2023-08-29 23:35:06 +0200

    MongoDB mit server verbindung hergestellt

---
+++ Server/routes/db.js
@@ -0,0 +1,10 @@
+// Hier wird eine Verbindung zur MongoDB-Datenbank aufgebaut.
+
+const mongoose = require('mongoose');
+
+
+
+mongoose.connect('mongodb://localhost:27017/my-loved-planet', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
--- Server/server.js
+++ Server/server.js
@@ -1,25 +1,25 @@
 
-const express = require('express');
-const cors = require('cors');
-const api = require('./routes/api');
-const app = express();
-const port = 8080; app.use(cors()); 
+// const express = require('express');
+// const cors = require('cors');
+// const api = require('./routes/api');
+// const app = express();
+// const port = 8080; app.use(cors()); 
 
 
-app.use('/api', api);
+// app.use('/api', api);
 
 
-/*
-app.get('/'): 
-Hier wird eine HTTP-GET-Anfrage auf der Wurzelroute ("/") definiert. 
-Das bedeutet, dass, wenn jemand die Haupt-URL der Website besucht (z.B. http://www.example.com/), dieser Codeblock ausgeführt wird.
-*/
+// /*
+// app.get('/'): 
+// Hier wird eine HTTP-GET-Anfrage auf der Wurzelroute ("/") definiert. 
+// Das bedeutet, dass, wenn jemand die Haupt-URL der Website besucht (z.B. http://www.example.com/), dieser Codeblock ausgeführt wird.
+// */
 
-app.get('/', (req, res) => {
-res.send('Hello von Express');
-});
+// app.get('/', (req, res) => {
+// res.send('Hello von Express');
+// });
 
 
-app.listen(port, () => {
-console.log(`Server läuft auf Port ${port}`);
-});
+// app.listen(port, () => {
+// console.log(`Server läuft auf Port ${port}`);
+// });
