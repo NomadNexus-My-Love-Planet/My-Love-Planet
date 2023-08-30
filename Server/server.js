6118105d9a866ab783dc69ef4dc26beef8b5ef07
Mohanad Ilewi <mohanad.ilewi.83@gmail.com>
2023-08-30 16:06:53 +0200

    MongoDB mit server verbindung hergestellt

--- Server/server.js
+++ Server/server.js
@@ -1,25 +1,87 @@
 
+// const express = require('express');
+// const mongoos = require('mongoose')
+// const app = express()
+
+// const port = 3001
+
+// // const mongodb = "mongodb+srv://NomadNexus:<password>@my-love-planet.vco2jng.mongodb.net/?retryWrites=true&w=majority"
+
+// async function run() {  
+//     await mongoos.connect('mongodb+srv://NomadNexus:<password>@my-love-planet.vco2jng.mongodb.net/?retryWrites=true&w=majority');
+
+//     const UserSchema = await mongoos.Schema({
+//     name: String,
+//     age: Number
+//     })
+
+//     const User = mongoos.model('User', UserSchema);
+
+//     await User.create({
+//     name: 'Test',
+//     age: 25
+//     })
+// }
+// run()
+// app.get('/', (req, res) => {
+    
+
+//     res.send('Hello von NomadNexus');
+// });
+
+
+// app.listen(port, () => {
+//         console.log(`Server is Running on ${port}`);
+
+// });
+
 const express = require('express');
-const cors = require('cors');
-const api = require('./routes/api');
+const mongoose = require('mongoose');
 const app = express();
-const port = 8080; app.use(cors()); 
 
+const port = 8080;
 
-app.use('/api', api);
+// Markieren Sie die run-Funktion als async
+async function run() {
+    try {
+        
 
+        await mongoose.connect('mongodb+srv://NomadNexus:techstarter5@my-love-planet.vco2jng.mongodb.net/Maps?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
 
-/*
-app.get('/'): 
-Hier wird eine HTTP-GET-Anfrage auf der Wurzelroute ("/") definiert. 
-Das bedeutet, dass, wenn jemand die Haupt-URL der Website besucht (z.B. http://www.example.com/), dieser Codeblock ausgeführt wird.
-*/
+        const UserSchema = new mongoose.Schema({
+            name: String,
+            age: Number
+        });
+
+        const User = mongoose.model('User', UserSchema);
+
+        const newUser = new User({
+            name: 'Test',
+            age: 40
+        });
+
+        await newUser.save();
+
+        console.log('User created successfully');
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+// Rufen Sie die run-Funktion auf
+run();
 
 app.get('/', (req, res) => {
-res.send('Hello von Express');
+    res.send('Hello von NomadNexus');
 });
-
 
 app.listen(port, () => {
-console.log(`Server läuft auf Port ${port}`);
+    console.log(`Server is running on ${port}`);
 });
+
+
+
+
