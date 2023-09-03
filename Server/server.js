11a8fe68f3b7055922018db34e275b9c1b04791c
Mohanad Ilewi <mohanad.ilewi.83@gmail.com>
2023-09-03 15:21:47 +0200

    Backend Komplett Update

--- Server/server.js
+++ Server/server.js
@@ -1,48 +1,82 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Place = require('./Models/place')
+const db = require("./db");
 const cors = require('cors');
-const app = express();
 
+
+const app = express();
 const port = 8080;
 
-// Markieren Sie die run-Funktion als async
-async function run() {
-    try {
-        
+app.use(cors());
+app.use(bodyParser.json());
 
-        await mongoose.connect('mongodb+srv://NomadNexus:techstarter5@my-love-planet.vco2jng.mongodb.net/Maps?retryWrites=true&w=majority', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+// Body Parser Middleware (für POST-Anfragen)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-        const MapsSchema = new mongoose.Schema({
-            city: String,
-            state: String,
-            postalcode: Number
-        });
-
-        const Maps = mongoose.model('Maps', MapsSchema);
-
-        const newMaps = new Maps({
-            city: 'Meschede',
-            state: 'NRW',
-            postalcode: 59872
-        });
-
-        await newMaps.save();
-
-        console.log('Maps Created Successfully');
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-// Rufen Sie die run-Funktion auf
-run();
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.get('/', (req, res) => {
-    res.send('Hello von NomadNexus');
+    res.send('Hello from everywhere');
 });
+
+app.post('/add', (req, res) => {
+    Place.create ({
+        name: req.body.name,
+        country : req.body.country,
+        reminder: req.body.reminder,
+        note:  req.body. note,
+        visit: req.body.visit
+    }).then((newplace)=>{res.send(newplace)})
+
+    .catch(err => res.send(err))
+});
+
+// Mussen wir noch anpassen
+
+app.post('/ortdaten', (req, res) => {
+    Place.create ({
+        name: req.body.name,
+        country : req.body.country,
+        reminder: req.body.reminder,
+        note:  req.body. note,
+        visit: req.body.visit
+    }).then((newplace)=>{res.send(newplace)})
+    
+    .catch(err => res.send(err))
+});
+
+
+
+// Neuen Ort hinzufügen
+app.post('/orte', async (req, res) => {
+try {
+    const {
+    name,
+    country,
+    city,
+    visitedDate,
+    reminder,
+    linkedPlaces,
+    } = req.body;
+    const neuerOrt = new Ort({
+    name,
+    country,
+    city,
+    visitedDate,
+    reminder,
+    linkedPlaces,
+    });
+    const gespeicherterOrt = await neuerOrt.save();
+    res.status(201).json(gespeicherterOrt);
+} catch (error) {
+    res.status(500).json({ error: error.message });
+}
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
